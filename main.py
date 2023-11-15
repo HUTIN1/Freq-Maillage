46efa126c002028ea66fc30343f7691fff193a7c
hutin <hutin.nathan1@gmail.com>
2023-11-15 21:50:44 +0100

    add : change example

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 import time
 
 start = time.time()
-path = os.path.normpath('C:\Ecole\Master_ID3D\Frequentielle\Freq-Maillage\data/robot.obj')
+path = os.path.normpath('C:\Ecole\Master_ID3D\Frequentielle\Freq-Maillage\data/bigguy.obj')
 # path = os.path.normpath('C:\Ecole\Master_ID3D\Frequentielle\Freq-Maillage\data/armadillo_light.off')
 
 v , f = utils.ReadObj(path)
@@ -13,9 +13,11 @@ print(f'v shape : {v.shape}, f shape {f.shape}')
 color = laplacian.diffusion_maps(v,f)
 
 L = laplacian.graph_laplacian(v,f)
-v = L @ v
+vfreq = L @ v
 
 display.display(v,f,color=color)
+
+display.display(vfreq,f)
 
 
 end = time.time()
